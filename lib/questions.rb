ad6061a50bd645ee43de6fbe036e360ef199f974
jamie rose <jrose111284@gmail.com>
2016-04-21 10:24:23 +0100

    double_array

--- lib/questions.rb
+++ lib/questions.rb
@@ -103,6 +103,7 @@ end
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+  array.concat(array).flatten
 end
 
 # convert a symbol into a string
